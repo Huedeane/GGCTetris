a5470ce1bc03c07ee7c5b526efb13ecc40e8fe13
Merchant151 <32748201+Merchant151@users.noreply.github.com>
2017-12-22 11:58:37 -0500

    added basic control
    Renamed The shapes giving them the class shape adding a script to give basic control to the shape moves left right and down

---
+++ Assets/sripts/Shape.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shape : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CheckUserInput ();
+	}
+    void CheckUserInput () {
+
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+
+            transform.position += new Vector3(1, 0, 0);
+
+        } else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+
+            transform.position += new Vector3(-1, 0, 0);
+                
+        } else if (Input.GetKeyDown(KeyCode.UpArrow)) {
+
+
+        } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+
+           transform.position += new Vector3(0, -1, 0);
+
+        }
+    }
+}
