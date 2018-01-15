a012dd5298c6845e8fb1035909ac9689df1deaae
Merchant151 <32748201+Merchant151@users.noreply.github.com>
2018-01-15 15:46:15 -0500

    Finished most of the game
    tetris works the way it should; peices fall, rows delete when full, etc
    tasks remaining:
    Score
    Game over
    main menu
    possibly do a preview for next peice box

--- Assets/sripts/Shape.cs
+++ Assets/sripts/Shape.cs
@@ -2,60 +2,137 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shape : MonoBehaviour {
+public class Shape : MonoBehaviour
+{
 
     float fall = 0;
     public float fallspeed = 1;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        CheckUserInput ();
-	}
-    void CheckUserInput () {
+    public bool allowRotation = true;
+    public bool limitRotation = false;
 
-        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("startShapeHasBeenCalled");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckUserInput();
+    }
+    void CheckUserInput()
+    {
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
 
             transform.position += new Vector3(1, 0, 0);
             if (CheckIsValidPosition())
             {
-
-            } else
+                FindObjectOfType<game>().UpdateGrid(this);
+            }
+            else
             {
                 transform.position += new Vector3(-1, 0, 0);
             }
 
-        } else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
 
             transform.position += new Vector3(-1, 0, 0);
             if (CheckIsValidPosition())
             {
-
-            } else
+                FindObjectOfType<game>().UpdateGrid(this);
+            }
+            else
             {
                 transform.position += new Vector3(1, 0, 0);
             }
-        } else if (Input.GetKeyDown(KeyCode.UpArrow)) {
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (allowRotation)
+            {
+                if (limitRotation)
+                {
+                    if (transform.rotation.eulerAngles.z >= 90)
+                    {
+                        transform.Rotate(0, 0, -90);
+                        if (CheckIsValidPosition())
+                        {
+                            FindObjectOfType<game>().UpdateGrid(this);
+                        }
+                        else
+                        {
+                            transform.Rotate(0, 0, 90);
+                        }
+                    }
+                    else
+                    {
+                        transform.Rotate(0, 0, 90);
+                        if (CheckIsValidPosition())
+                        {
+                            FindObjectOfType<game>().UpdateGrid(this);
+                        }
+                        else
+                        {
+                            transform.Rotate(0, 0, 90);
+                        }
+                    }
+                }
+                else
+                {
 
-            transform.Rotate (0, 0, 90);
 
-        } else if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - fall >= fallspeed) {
+                    transform.Rotate(0, 0, 90);
+                    if (CheckIsValidPosition())
+                    {
+                        FindObjectOfType<game>().UpdateGrid(this);
+                    }
+                    else
+                    {
+                        transform.Rotate(0, 0, -90);
+                    }
+                }
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - fall >= fallspeed)
+        {
 
-           transform.position += new Vector3(0, -1, 0);
+            transform.position += new Vector3(0, -1, 0);
+            if (CheckIsValidPosition())
+            {
+                FindObjectOfType<game>().UpdateGrid(this);
+            }
+            else
+            {
+                transform.position += new Vector3(0, 1, 0);
+
+                FindObjectOfType<game>().DeleteRow();
+
+                enabled = false;
+
+                FindObjectOfType<game>().SpawnNextShape();
+            }
+
 
             fall = Time.time;
         }
     }
-    bool CheckIsValidPosition ()
+    bool CheckIsValidPosition()
     {
         foreach (Transform mino in transform)
         {
             Vector2 pos = FindObjectOfType<game>().Round(mino.position);
-            if (FindObjectOfType<game>().CheckIsInsideGrid (pos) == false)
+
+            if (FindObjectOfType<game>().CheckIsInsideGrid(pos) == false)
+            {
+                return false;
+            }
+            if (FindObjectOfType<game>().GetTransformAtGridPosition(pos) != null && FindObjectOfType<game>().GetTransformAtGridPosition(pos).parent != transform)
             {
                 return false;
             }
--- Assets/sripts/game.cs
+++ Assets/sripts/game.cs
@@ -2,28 +2,148 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class game : MonoBehaviour {
+public class game : MonoBehaviour
+{
 
     public static int gridwidth = 10;
-    public static int gridhight = 20;
+    public static int gridheight = 20;
+    public static Transform[,] grid = new Transform[gridwidth, gridheight];
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("startstart");
+        SpawnNextShape();
+        Debug.Log("startend");
+    }
+    public bool IsFullRowAt(int y)
+    {
+        for (int x = 0; x < gridwidth; ++x)
+        {
+            if (grid[x, y] == null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public void DeleteMinoAt(int y)
+    {
+        for (int x = 0; x < gridwidth; ++x)
+        {
+            Destroy(grid[x, y].gameObject);
+            grid[x, y] = null;
+        }
+    }
+    public void MoveRowDown(int y)
+    {
+        for (int x = 0; x < gridwidth; ++x)
+        {
+            if (grid[x, y] != null)
+            {
+                grid[x, y - 1] = grid[x, y];
+                grid[x, y] = null;
+                grid[x, y - 1].position += new Vector3(0, -1, 0); 
+            }
+        }
+    }
+    public void MoveAllRowsDown(int y)
+    {
+        for (int i = y; i < gridheight; ++i)
+        {
+            MoveRowDown(i);
+        }
+    }
+    public void DeleteRow()
+    {
+        for (int y = 0; y < gridheight; ++y)
+        {
+            if (IsFullRowAt(y))
+            {
+                DeleteMinoAt(y);
+                MoveAllRowsDown(y + 1);
+                --y;
+            }
+        }
+    }
+    public void UpdateGrid(Shape shape)
+    {
+        for (int y = 0; y < gridheight; ++y)
+        {
+            for (int x = 0; x < gridwidth; ++x)
+            {
+                if (grid[x, y] != null)
+                {
+                    if (grid[x, y].parent == shape.transform)
+                    {
+                        grid[x, y] = null;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    public bool CheckIsInsideGrid(Vector2 pos) {
+                    }
+                }
+            }
+        }
+        foreach (Transform mino in shape.transform)
+        {
+            Vector2 pos = Round(mino.position);
+            if (pos.y < gridheight)
+            {
+                grid[(int)pos.x, (int)pos.y] = mino;
+            }
+        }
+    }
+    public Transform GetTransformAtGridPosition(Vector2 pos)
+    {
+        if (pos.y > gridheight - 1)
+        {
+            return null;
+        }
+        else
+        {
+            return grid[(int)pos.x, (int)pos.y];
+        }
+    }
+    public void SpawnNextShape()
+    {
+        Debug.Log("nextshapewillbecalled");
+        GameObject nextShape = (GameObject)Instantiate(Resources.Load(GetRandomShape(), typeof(GameObject)), new Vector2(5.0f, 20.0f), Quaternion.identity);
+    }
+    public bool CheckIsInsideGrid(Vector2 pos)
+    {
 
         return ((int)pos.x >= 0 && (int)pos.x < gridwidth && (int)pos.y >= 0);
     }
-    public Vector2 Round (Vector2 pos)
+    public Vector2 Round(Vector2 pos)
     {
-        return new Vector2 (Mathf.Round(pos.x), (Mathf.Round(pos.y)));
+        return new Vector2(Mathf.Round(pos.x), Mathf.Round(pos.y));
     }
-}
+    string GetRandomShape()
+    {
+        int randomShape = Random.Range(1, 8);
+        string randomShapeName = "prefabs/Shape_T";
+        switch (randomShape)
+        {
+            case 1:
+                randomShapeName = "prefabs/Shape_T";
+                break;
+            case 2:
+                randomShapeName = "prefabs/Shape_J";
+                break;
+            case 3:
+                randomShapeName = "prefabs/Shape_L";
+                break;
+            case 4:
+                randomShapeName = "prefabs/Shape_Long";
+                break;
+            case 5:
+                randomShapeName = "prefabs/Shape_S";
+                break;
+            case 6:
+                randomShapeName = "prefabs/Shape_Square";
+                break;
+            case 7:
+                randomShapeName = "prefabs/Shape_Z";
+                break;
+        }
+        return randomShapeName;
+    }
 
+}
